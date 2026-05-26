b05d7a19da915a992d9e778c302c3d4dfbc238d5
JoeTech <155426056+joeviral@users.noreply.github.com>
2026-05-26 15:55:40 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+// LOADER
+
+window.addEventListener("load", () => {
+
+  const loader = document.getElementById("loader");
+
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 2000);
+
+});
+
+// MOBILE MENU
+
+const menuBtn = document.querySelector(".menu-btn");
+const navLinks = document.querySelector(".nav-links");
+
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// TESTIMONIAL SLIDER
+
+const testimonials = document.querySelectorAll(".testimonial");
+
+let current = 0;
+
+function showTestimonial(){
+
+  testimonials.forEach((testimonial) => {
+    testimonial.classList.remove("active");
+  });
+
+  testimonials[current].classList.add("active");
+
+  current++;
+
+  if(current >= testimonials.length){
+    current = 0;
+  }
+
+}
+
+setInterval(showTestimonial, 10000);
